4bb086886106143b889374432dfb53447a1997f1
Dinanath Maurya <88203619+dnmm@users.noreply.github.com>
2021-08-27 15:17:07 +0530

    new commit

---
+++ SwiftTraining/9_aug .swift
@@ -0,0 +1,8 @@
+//
+//  9_aug .swift
+//  SwiftTraining
+//
+//  Created by Dinanath Maurya on 09/08/21.
+//
+
+//import Foundation
--- SwiftTraining/main.swift
+++ SwiftTraining/main.swift
@@ -5,7 +5,41 @@
 //  Created by Dinanath Maurya on 06/08/21.
 //
 
-import Foundation
+//func bubbleSort (arr: [Int])-> [Int]
+//{
+//var array = arr
+//for _ in 0..<array.count-1
+//{
+//    for j in 0..<array.count-1
+//    {
+//        if(array[j] > array[j+1])
+//        {
+//            let temp = array[j]
+//            array[j] = array[j+1]
+//            array[j+1] = temp
+//            
+//        }
+//    }
+//}
+//return array
+//}
+//let unsortedArray = [4,3,8,1,7,0]
+//print(bubbleSort(arr: unsortedArray))
 
-print("Hello, World!")
-
+let num1 = 40
+let num2 = 20
+if num1 > num2
+{
+    print("\(num1) is greater than \(num1),\(num2)")
+}
+else if (num2 > num1)
+{
+    print("\(num2) is greater than \(num1),\(num2)")
+    
+    
+ 
+}
+else
+{
+    print("\(num1) is equal to \(num2)")
+}
