e3a9cb4f3111ef7681132e9b91381ce8b8370ebb
Dinanath Maurya <88203619+dnmm@users.noreply.github.com>
2021-09-03 14:00:24 +0530

    Formatting

--- SwiftTraining/9_aug .swift
+++
@@ -1,8 +0,0 @@
-//
-//  9_aug .swift
-//  SwiftTraining
-//
-//  Created by Dinanath Maurya on 09/08/21.
-//
-
-//import Foundation
---
+++ SwiftTraining/FindGraterNumber.swift
@@ -0,0 +1,17 @@
+func greater() {
+    let num1 = 40
+    let num2 = 20
+    if num1 > num2
+    {
+        print("\(num1) is greater than \(num1),\(num2)")
+    }
+    else if (num2 > num1)
+    {
+        print("\(num2) is greater than \(num1),\(num2)")
+       
+    }
+    else
+    {
+        print("\(num1) is equal to \(num2)")
+    }
+ }
